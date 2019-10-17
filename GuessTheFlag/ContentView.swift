01f7cede10c79db4c421b70fb9561fe9460fa0ab
Peter Salz <psalz@web.de>
2019-10-17 13:10:17 +0200

    Colors and frames

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -12,41 +12,11 @@ struct ContentView: View {
     
     var body: some View {
         
-        /*
-        VStack(spacing: 20) {
-            Text("First")
-            Text("Second")
-            Text("Third")
-            Spacer()
+        ZStack {
+            Color.red.edgesIgnoringSafeArea(.all)
+            Text("Your Content")
         }
-        */
-        
-        /*
-        ZStack(alignment: .top) {
-            Text("Hello World")
-            Text("This is inside a stack")
-        }*/
-        
-        VStack(spacing: 20) {
-            
-            HStack(spacing: 20) {
-                Text("1")
-                Text("2")
-                Text("3")
-            }
-            
-            HStack(spacing: 20) {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            
-            HStack(spacing: 20) {
-                Text("7")
-                Text("8")
-                Text("9")
-            }
-        }
+        .background(Color.red)
     }
 }
 
